e7bab771cd6e18f4414fbb8f8e5129d1c633c047
sumitbasu116@gmail.com <sumitbasu116@gmail.com>
2025-03-04 15:35:19 +0530

    excercise adding task component for a user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,16 +5,23 @@ import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 
 import { DUMMY_USERS } from './dummy-users';
+import { TaskComponent } from "./task/task.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent,UserComponent],
+  imports: [HeaderComponent, UserComponent, TaskComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   users = DUMMY_USERS;
-  onSelectUser(id:string){
-    console.log('Selected user id:'+id);
+  selectedId='u1';
+
+  get selectedUser(){
+    return this.users.find((user)=>user.id === this.selectedId)!;
+  }
+
+  onSelectUser(id: string){
+    this.selectedId=id;
   }
 }
---
+++ src/app/task/task.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-task',
+  imports: [],
+  templateUrl: './task.component.html',
+  styleUrl: './task.component.css'
+})
+export class TaskComponent {
+
+  @Input() userName!:string;
+}
