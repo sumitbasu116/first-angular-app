9e894dbd83736d293144d5070f42393d34912b59
sumitbasu116@gmail.com <sumitbasu116@gmail.com>
2025-03-11 23:36:33 +0530

    two way binding add task form 1

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,14 +1,19 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
 
   @Output() doCancel = new EventEmitter();
+
+  enteredTitle = '';
+  enteredSummary = '';
+  eneteredDate = '';
 
   cancelAddTaskWindowEmitEvent(){
     this.doCancel.emit();
