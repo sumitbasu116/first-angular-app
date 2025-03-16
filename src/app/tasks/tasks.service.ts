4c392609275f3224301d167f2bf3a6aa33f06ecd
sumitbasu116@gmail.com <sumitbasu116@gmail.com>
2025-03-16 13:50:48 +0530

    Using localStorage for Data Storage

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -36,6 +36,13 @@ export class TasksService{
         }
       ];
 
+      constructor(){
+        const tasks = localStorage.getItem('tasks');
+        if(tasks){
+          this.tasks = JSON.parse(tasks);
+        }
+      }
+
       getUserTasks(userId:string){
         return this.tasks.filter((task)=>task.userId === userId);
       }
@@ -48,10 +55,16 @@ export class TasksService{
             summary:taskData.summary,
             dueDate:taskData.dueDate,
           });
+          this.saveTasks();
       }
 
       removeTask(id: string){
-        this.tasks=this.tasks.filter((task)=>id!==task.id)
+        this.tasks=this.tasks.filter((task)=>id!==task.id);
+        this.saveTasks();
+      }
+
+      private saveTasks(){
+        localStorage.setItem('tasks',JSON.stringify(this.tasks));
       }
 
 }
