3442840b6c57066c9cf8d89f2f545e110a4beb24
sumitbasu116@gmail.com <sumitbasu116@gmail.com>
2025-03-17 00:18:29 +0530

    creating a shared module for CardComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,18 @@ import { AppComponent } from './app.component'
 import { HeaderComponent } from './header/header.component'
 import { UserComponent } from './user/user.component'
 import { TasksComponent } from './tasks/tasks.component'
-import { CardComponent } from './shared/card/card.component'
 import { NewTaskComponent } from './tasks/new-task/new-task.component'
 import { TaskComponent } from './tasks/task/task.component'
+import { SharedModule } from './shared/shared.module'
 
 
 @NgModule({
 
-    declarations: [AppComponent,HeaderComponent,UserComponent,CardComponent,TasksComponent,
+    declarations: [AppComponent,HeaderComponent,UserComponent,TasksComponent,
         TaskComponent,NewTaskComponent
     ],
     bootstrap: [AppComponent],
-    imports: [BrowserModule,FormsModule]
+    imports: [BrowserModule,FormsModule,SharedModule]
 })
 
 export class AppModule {
